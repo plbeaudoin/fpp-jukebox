da21d07e4d39c58d944adc3476d66ed9b06a5418
Pierre-Luc Beaudoin <pierre-luc@pierlux.com>
2024-11-16 16:45:45 -0500

    Fix class vs className

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -34,11 +34,11 @@ export default function Home() {
 
             Comment ça marche?
 
-            {!isInstructionsVisible && (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-6">
+            {!isInstructionsVisible && (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
             </svg>)}
 
-            {isInstructionsVisible && (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-6">
+            {isInstructionsVisible && (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
             </svg>)}
 
